249a8eb7088a070e31707ab66cfa7529be9864a1
WEMrxiaobai <1473674817@qq.com>
2022-06-12 11:44:19 +0800

    test

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 // config 文件  需要修改 imgBaseUrl地址 和 ipPort地址
 web_config = {
-    secretKey: 'userlogintoken123456',// 秘钥 secretKey
-    DesTime: 60 * 60 * 2,// token 过期时间  DesTime
+    secretKey: 'userlogintoken1234567890',// 秘钥 secretKey
+    DesTime: 60 * 60 * 3,// token 过期时间  DesTime
     imgBaseUrl: '', 
     ipPort: '',
 }
